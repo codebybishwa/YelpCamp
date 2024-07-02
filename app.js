998740ea011bfa677d277ab5b375cad257d34ecb
Bishwamohan Jena <jenabishwamohan@gmail.com>
2024-07-02 21:08:44 +0530

    basic crud operations done

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const Campground = require('./models/campground')
+const Campground = require('./models/campground'); 
+const methodOverride = require('method-override');
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 const mongoose = require('mongoose');
 
@@ -20,11 +23,43 @@ app.get('/', (req, res) => {
     res.send("hello from yelpcamp");
 })
 
-app.get('/makeCampground', async (req, res) => {
-    const camp = new Campground({title: "My home"});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
 })
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect('/campgrounds');
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
 
 app.listen(3000, () => {
     console.log("Server Working!!!");
