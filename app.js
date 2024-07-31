95d883e57a0abaf9837f6812e378bf1b7ff49d00
Bishwamohan Jena <jenabishwamohan@gmail.com>
2024-07-31 19:46:25 +0530

    client and server side validation done

--- app.js
+++ app.js
@@ -1,66 +1,133 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const path = require('path');
-const Campground = require('./models/campground'); 
+const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const {campgroundSchema} = require('./schemas');
+const campground = require('./models/campground');
+
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
-const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
+
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
   console.log("Database working fine");
 }
 
+const validateCampground = (req, res, next) => {
+  
+  const {error} = campgroundSchema.validate(req.body);
+  if(error) {
+     const msg = error.details.map(element => element.message).join(',');
+     throw new ExpressError(msg, 400);
+  }
+  else {
+    next();
+  }
+}
+
 
 app.get('/', (req, res) => {
-    res.send("hello from yelpcamp");
-})
+  res.send("Hello from YelpCamp");
+});
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-})
+
+app.get('/campgrounds', catchAsync(async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+}));
+
 
 app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-})
+  res.render('campgrounds/new');
+});
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect('/campgrounds');
-})
 
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground});
-})
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+  if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground});
-})
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect('/campgrounds');
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
-})
 
-app.delete('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send('Invalid ID format');
+  }
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    return res.status(404).send('Campground not found');
+  }
+  res.render('campgrounds/show', { campground });
+}));
+
+
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send('Invalid ID format');
+  }
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    return res.status(404).send('Campground not found');
+  }
+  res.render('campgrounds/edit', { campground });
+}));
+
+
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send('Invalid ID format');
+  }
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  if (!campground) {
+    return res.status(404).send('Campground not found');
+  }
+  res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+
+app.delete('/campgrounds/:id', catchAsync(
+  async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send('Invalid ID format');
+    }
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+  }
+));
+
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError('Page not found!!', 404));
+})
+
+
+app.use((err, req, res, next) => {
+  const {statusCode = 500} = err;
+  if(!err.message) err.message = "Something went wrong";
+
+  res.status(statusCode).render('error', {err});
 })
 
 
 app.listen(3000, () => {
-    console.log("Server Working!!!");
-})
+  console.log("Server working!!!");
+});
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities.js');
-const {places, descriptors} = require('./seedHelpers.js')
+const { places, descriptors } = require('./seedHelpers.js')
 const Campground = require('../models/campground');
 
 main().catch(err => console.log(err));
@@ -12,18 +12,22 @@ async function main() {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async() => {
-   await Campground.deleteMany({});
-   for(let i = 0; i < 50; i++) {
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 30;
     const camp = new Campground({
-        location: `${cities[random1000].city}, ${cities[random1000].state}`,
-        title: `${sample(descriptors)} ${sample(places)}`
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: "The image captures a serene, idyllic countryside scene at the break of dawn. The sky, painted in a gradient of deep purples fading into soft pinks and oranges, heralds the beginning of a new day. The sun, a glowing orb just peeking over the horizon, casts a golden hue across the landscape, creating a stunning contrast with the cool colors of the early morning sky.",
+      price
     })
     await camp.save();
-   }
+  }
 }
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
