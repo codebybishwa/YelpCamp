de8c214d07fbbdef781fdc252a81371b803c2cc9
Bishwamohan Jena <jenabishwamohan@gmail.com>
2024-09-30 11:59:23 +0530

    recent changes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-  console.log("Database working fine");
+  console.log("Database is working fine");
 }
 
 const validateCampground = (req, res, next) => {
